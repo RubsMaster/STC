7a5c75e077dae15ddbf2fb6e55d784f4d2497154
Jael Hernandez <jael.hernandez@madata.com>
2020-11-24 20:15:15 -0700

    Merge de rama feature/register

--- logInSucces.php
+++ logInSucces.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 
@@ -20,27 +19,4 @@ if ($email = mysqli_fetch_assoc($query)) {
 }
 
 
-=======
-<?php
-session_start();
-
-require_once 'connect.php';
-
-$email = $_POST['user'];
-$pass1 = $_POST['pass1'];
-
-$query = mysqli_query($conexion, "SELECT * FROM usuarios WHERE email = '$email' AND pass1 = '$pass1'");
-if (!$query) {
-    header("location: index.php");
-    echo mysqli_error($mysqli);
-}
-
-if ($email = mysqli_fetch_assoc($query)) {
-    header("location: home.php");
-} else {
-    header("location: index.php");
-}
-
-
->>>>>>> feature/register
 ?>
